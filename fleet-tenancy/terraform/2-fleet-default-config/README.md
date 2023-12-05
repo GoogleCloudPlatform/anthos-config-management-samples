a2a25fdb9b9583c0c93ea0246f79282d15912931
Nan Yu <nanyu@google.com>
2023-12-05 23:48:17 +0000

    Add TF configs to configure Fleet Default Member Config for Config Management

---
+++ fleet-tenancy/terraform/2-fleet-default-config/README.md
@@ -0,0 +1,19 @@
+# Configure Fleet Default Member Config
+
+The Terraform configurations in this directory configures the fleet default member config for Config Management
+
+## Usage
+
+See the [variable definitions file] for an exhaustive list of variables.
+These can be provided as [command line variables] at runtime.
+
+For example:
+```shell
+export TF_VAR_project=your-gcp-project
+export TF_VAR_sa_key_file=path/to/your-sa-key-file
+terraform init
+terraform plan
+terraform apply
+```
+
+[variable definitions file]: ./variables.tf
---
+++ fleet-tenancy/terraform/2-fleet-default-config/main.tf
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+terraform {
+  required_providers {
+    google-beta = {
+      source = "hashicorp/google-beta"
+      version = "5.8.0"
+    }
+  }
+}
+
+provider "google-beta" {
+  credentials = var.sa_key_file
+  project = var.project
+}
+
+resource "google_gke_hub_feature" "feature" {
+  name = "configmanagement"
+  location = "global"
+  provider = google-beta
+  fleet_default_member_config {
+    configmanagement {
+      # version = "1.17.0" # Use the default latest version instead of 1.17.0 because the version field is mislocated: b/314995011
+      config_sync {
+        source_format = "unstructured"
+        git {
+          sync_repo = "https://github.com/nan-yu/anthos-config-management-samples"
+          sync_branch = "main"
+          policy_dir = "fleet-tenancy/config"
+          secret_type = "none"
+        }
+      }
+    }
+  }
+}
