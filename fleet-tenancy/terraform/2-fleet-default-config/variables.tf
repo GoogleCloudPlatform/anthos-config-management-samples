50654641800edbf6713012d33781cb7b51837bf4
tay <45858469+tarynlucas@users.noreply.github.com>
2024-02-23 12:04:14 -0500

    region comments in variables.tf

--- fleet-tenancy/terraform/2-fleet-default-config/variables.tf
+++ fleet-tenancy/terraform/2-fleet-default-config/variables.tf
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+# [START fleet_default_variables]
 variable "project" {
   type = string
   description = "Name of GCP project"
@@ -23,3 +23,4 @@ variable "sa_key_file" {
   type = string
   description = "Path to the GCP service account's private key JSON file"
 }
+# [END fleet_default_variables]
